2ab617ef923c81a0404ba2f49e5fc3724f4136e6
Ephantus Wambui <ephywambui72@gmail.com>
2021-09-01 18:31:00 +0300

    added fullname and label

--- models/Users.js
+++ models/Users.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
+const slugify = require('slugify');
 
 const bcrypt = require('bcrypt')
 
 const UserSchema = new Schema({
     email: String,
+    fullname: String,
     pwd: String,
     dateJoined: {
         type: Date,
@@ -19,6 +21,14 @@ UserSchema.pre('save', function(next) {
         user.pwd = hash
         next()
     })
+})
+
+UserSchema.pre('validate', function (next) {
+  if (this.fullname) {
+    this.slug = slugify(this.fullname, { lower: true, strict: true })
+  }
+  
+  next()
 })
 
 const User = mongoose.model('User', UserSchema);
