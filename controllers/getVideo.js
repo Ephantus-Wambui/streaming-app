14c3be8fb4db0aadc578c701e8c0b4a43f232e8f
Ephantus Wambui <58903181+Ephantus-Wambui@users.noreply.github.com>
2022-03-13 10:03:27 +0300

    enabled showing of other videos to stream

--- controllers/getVideo.js
+++ controllers/getVideo.js
@@ -1,10 +1,12 @@
 const Youtube =require('../models/Youtube')
 
 module.exports = async (req, res) => {
-    const youtube = await Youtube.findOne({ slug: req.params.slug }).populate('userid')
+    const yoUtube = await Youtube.findOne({ slug: req.params.slug }).populate('userid')
+    const youtube = await Youtube.find()
     console.log(youtube)
     res.render('viewVideo',{
         viewVideo: true,
+        yoUtube,
         youtube
     });
 }
